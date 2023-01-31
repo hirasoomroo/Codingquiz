8823d1050a7f2ddfa82d8300dc9ed82e5a32c440
Hira Soomro <hira@Hiras-MacBook-Pro.local>
2023-01-31 11:47:22 -0800

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,31 @@
-var quizBody = document.querySelector(".quiz");
-var timerforQuiz = document.querySelector (".timercount");
-var startButton = document.querySelector(".start-button");
-var gameStart= document.querySelector(".startgame");
-var highscorepagediv = document.querySelector(".highscorepage");
-var highscoreheaderdiv = document.querySelector(".highscoreheader");
-var startpagediv = document.querySelector(".start");
+
+var quizBody = document.getElementById("quiz");
+var timerforQuiz = document.getElementById ("timercount");
+var startButton = document.getElementById("startbutton");
+
+
+var startpagediv = document.getElementById("startpage");
+
 var buttonA = document.getElementById("a");
 var buttonB = document.getElementById("b");
 var buttonC = document.getElementById("c");
 var buttonD = document.getElementById("d");
 
-var gameoverdiv= document.querySelector(".gameover");
-var buttonsenddiv= document.querySelector(".buttonsend");
-var submitscore = document.querySelector(".submit");
-var highscoredisplay = document.querySelector(".Highscore!");
-var gameoverDisplay= document.querySelector(".gameover");
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('main');
+
+var questiondiv = document.querySelector("#question")
+var gameoverdiv= document.querySelector("#gameover");
+var buttonsenddiv= document.querySelector("#buttonsend");
+
+var resultsdiv= document.querySelector("#results");
 
 var quizQuestions = [{
-    question: "How many elements can you apply an 'ID' attribute to?",
-    choiceA: "As many as you want",
-    choiceB: "3",
-    choiceC: "1",
-    choiceD: "128",
+    question: "The If/else statement is enclosed in?",
+    choiceA: "square brackets",
+    choiceB: "parentheses",
+    choiceC: "curly brackets",
+    choiceD: "quotes",
     correctAnswer: "c"},
   {
     question: "What does DOM stand for?",
@@ -38,13 +42,6 @@ var quizQuestions = [{
     choiceD: "React.js",
     correctAnswer: "b"},
     {
-    question: "What HTML tags are JavaScript code wrapped in?",
-    choiceA: "&lt;div&gt;",
-    choiceB: "&lt;link&gt;",
-    choiceC: "&lt;head&gt;",
-    choiceD: "&lt;script&gt;",
-    correctAnswer: "d"},
-    {
     question: "When is localStorage data cleared?",
     choiceA: "No expiration time",
     choiceB: "On page reload",
@@ -59,60 +56,80 @@ var quizQuestions = [{
     choiceD: "Wendy Wants Waffles",
     correctAnswer: "c"},
     {
-    question: "What HTML attribute references an external JavaScript file?",
-    choiceA: "href",
-    choiceB: "src",
-    choiceC: "class",
-    choiceD: "index",
+    question: "What do we use to add an image?",
+    choiceA: "img href",
+    choiceB: "img src",
+    choiceC: "img class",
+    choiceD: "id",
     correctAnswer: "b"},
         
     
     ];
 
+var questionIndex = 0;
+
+// Timer that counts down from 75
+function countdown() {
+  var timeLeft = 75;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+     
+    }
+  }, 1000);
+}
+countdown();
+
 
 //function to generate quiz questions
-function generatequizquestion() {
-gameoverDisplay.style.display= "none";
-if (currentQuestion===finalQuestionIndex){
-    return showScore;
+function startQuiz(){ 
+  console.log("hello");
+  gameoverdiv.style.display = "none";
+  startpagediv.style.display = "none";
+
+  generateQuizQuestion();
+
 }
 
-var currentQuestion=0;
-var finalQuestionIndex = quizQuestions.length;
-var timerInterval;
-
-var currentQuestion = quizQuestions[currentQuestionIndex];
-questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
-buttonA.innerHTML = currentQuestion.choiceA;
-buttonB.innerHTML = currentQuestion.choiceB;
-buttonC.innerHTML = currentQuestion.choiceC;
-buttonD.innerHTML = currentQuestion.choiceD;
+function generateQuizQuestion() {
+  var currentquestion = quizQuestions [questionIndex];
+  questiondiv.textContent = currentquestion.question
+  buttonA.textContent= currentquestion.choiceA
+  buttonB.textContent = currentquestion.choiceB
+  buttonC.textContent= currentquestion.choiceC
+  buttonD.textContent = currentquestion.choiceD
 }
-startButton.addEventListener("click",startQuiz);
+function checkAnswer(choice) {
+console.log(choice);
+var answer = quizQuestions[questionIndex].correctAnswer
 
-function startQuiz(){
-    gameoverdiv.style.display = "none";
-    gameStart.style.display = "none";
-    generatequizquestion();
-
-    //timer
-    timerInterval = setInterval(function() {
-        timeLeft--;
-        quiztimercount.textContent = "Time left: " + timeLeft;
-
-        if(timeLeft === 0) {
-          clearInterval(timerInterval);
-          showScore();
-        }
-      }, 1000);
-    quizBody.style.display = "block";
+if (choice!== answer){
+//subtracts 10 seconds from timer
+  
 }
 
-function showScore(){
-    quizBody.style.display = "none"
-    gameoverDiv.style.display = "flex";
-    clearInterval(timerInterval);
-    highscoreInputName.value = "";
-    finalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+
+
+questionIndex ++ 
+generateQuizQuestion();
+
 }
 
+
+var finalquestion = 6;
+var finalquestion = quizQuestions 
